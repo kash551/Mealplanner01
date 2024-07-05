2acbfdff361fe86df92b558128596fd2ef550dd5
Kashif Hussain <kashifh2005@gmail.com>
2024-07-05 12:51:49 +0000

    added the view for the register page

--- home/views.py
+++ home/views.py
@@ -77,3 +77,23 @@ def login_page(request):
             messages.error(request, "Something went wrong")
             return redirect('/register/')
     return render(request, "login.html")    
+
+#register page for user
+def register_page(request):
+    if request.method == "POST":
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user_obj = User.objects.filter(username=username)
+            if user_obj.exists():
+                messages.error(request, "Username is taken")
+                return redirect('/register/')
+            user_obj = User.objects.create(username=username)
+            user_obj.set_password(password)
+            user_obj.save()
+            messages.success(request, "Account created")
+            return redirect('/login')
+        except Exception as e:
+            messages.error(request, "Something went wrong")
+            return redirect('/register')
+    return render(request, "register.html")    
