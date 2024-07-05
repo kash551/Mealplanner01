d6a0c62782d265e850d5f598397668cc959db2d4
Kashif Hussain <kashifh2005@gmail.com>
2024-07-05 12:48:23 +0000

    added the view for the recipe page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Recipe
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required 
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
-# Create your views here.
+# Create recipe page
+@login_required(login_url='/login/')
+def recipes(request):
+    if request.method == 'POST': 
+        data = request.POST
+        day = data.get('day')
+        name = data.get('name')
+        description = data.get('description')      
+        Recipe.objects.create(
+            day = day,
+            name=name,
+            description=description,        
+        )
+        return redirect('/')
+ 
+    queryset = Recipe.objects.all()
+    if request.GET.get('search'):
+        queryset = queryset.filter(
+            day__icontains=request.GET.get('search'))
+          
+    context = {'recipes': queryset}
+    return render(request, 'recipe.html', context)
